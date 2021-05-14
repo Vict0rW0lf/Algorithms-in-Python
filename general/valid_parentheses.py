1f7a29f4e5d1a89c9a1917b6fddc71de58350210
diowolf <diorgenes_tex@live.com>
2021-05-13 22:42:28 -0300

    Adds algo that checks whether parentheses are valid

---
+++ general/valid_parentheses.py
@@ -0,0 +1,13 @@
+#O(n) where N is the size of the String
+def valid_parenthesis(txt):
+  stack = []
+  types = {"}": "{", ")": "(", "]": "["}
+  
+  for letter in txt:
+   if letter in types.values():
+     stack.append(letter)
+   elif letter in types:
+     if len(stack) != 0 and stack.pop() != types[letter]:
+       return False
+
+  return len(stack) == 0
