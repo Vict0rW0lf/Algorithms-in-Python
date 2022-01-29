007fb76b40a9dd6c3eb59b9dfa196e5694262c3f
victorhunt3r <victor.s1lva@hotmail.com>
2022-01-29 17:54:41 -0300

    Adds optimized version of bubble sort

---
+++ sorting/bubble_sort_optimized.py
@@ -0,0 +1,19 @@
+def bubble_sort(arr):  
+  for i in range(len(arr)):
+    print(arr)
+    
+    for j in range(len(arr) - 1 - i):
+      if arr[j] > arr[j + 1]:
+        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        swap = True
+
+    if swap == False:
+      break;
+
+    swap = False
+
+arr = [0, -11, 2, 99, 100, 0, 0, 1, 230829]
+
+bubble_sort(arr)
+
+print(arr)
